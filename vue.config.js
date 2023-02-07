94e4aca41885c869ab32394d2e7f7d87daad0415
Dmitry Fedoseev <fedoseevdmitry0@gmail.com>
2023-02-07 11:26:32 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,5 @@ module.exports = defineConfig({
       }
     }
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/positron/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-cart/" : "/",
 })
